ab8c27b643e025754df8f328e71dba9152e338ae
Renato Bento <renatombento@hotmail.com>
2017-04-17 13:42:40 -0300

    javascript solucao

--- src/assets/js/validacao.js
+++ src/assets/js/validacao.js
@@ -1,12 +1,15 @@
     var itemprojeto = document.getElementsByClassName("itemprojeto")
-    
+    console.log(itemprojeto);
     function exibirUrl(e){
-        e.style.display = "none";
+        console.log('------------------------------------');
+        console.log(e.children);
+        console.log('------------------------------------');
+        e.children[0].style.display = "block";
     }
 
-    for(var i =0; i<itemprojeto.length;i++){
-        itemprojeto.addEventListener("click",exibirUrl(itemprojeto[i]),false);
-        
+    for(var i=0;i<itemprojeto.length;i++){
+      itemprojeto[i].onclick = function(){exibirUrl(this);}
     }
-    
+
+  
    
