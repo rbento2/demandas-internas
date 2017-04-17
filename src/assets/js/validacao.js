977a86f5ebffb4be58b14abd049e5ccbf1a6eb75
Renato Bento <renatombento@hotmail.com>
2017-04-17 11:32:49 -0300

    javascript

---
+++ src/assets/js/validacao.js
@@ -0,0 +1,7 @@
+window.addEventListener("load",function(){
+
+    document.getElementsByClassName("itemprojeto").addEventListener("click",function(){
+        document.getElementsByClassName("form -link").style.display = "block";
+    });
+
+});
