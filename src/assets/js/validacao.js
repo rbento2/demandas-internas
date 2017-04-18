80067310f54b564f536025c42e7f0af36c436b99
Renato Bento <renatombento@hotmail.com>
2017-04-17 22:58:59 -0300

    função de adicionar projetos incompleta

--- src/assets/js/validacao.js
+++ src/assets/js/validacao.js
@@ -1,18 +1,43 @@
-    var itemprojeto = document.getElementsByClassName("itemprojeto")
-    
-    function exibirUrl(e){
+    function exibirUrl(e) {
         var links = e.nextElementSibling;
-        if(links.style.display=="" || links.style.display=="none"){
+        if (links.style.display == "" || links.style.display == "none") {
             e.nextElementSibling.style.display = "block";
-        }else{
+        } else {
             e.nextElementSibling.style.display = "none";
         }
-        
     }
 
-    for(var i=0;i<itemprojeto.length;i++){
-      itemprojeto[i].onclick = function(){exibirUrl(this);}
+    function adicionaProjeto() {
+        var formlink = "<li class='itemprojeto -claro'>teste </li>";
+        formlink += "<section class='form -link'>";
+        formlink += "<ul class='listaatribo'>";
+        formlink += "<li class='atributo'>";
+        formlink += "<ul class='listalink'>";
+        formlink += "<li class='link'>Link de Dev </li>";
+        formlink += "<li class='link'>Link de Hmg </li>";
+        formlink += "<li class='link'>Link de Produção </li>"
+        formlink += "</ul>";
+        formlink += "</li>";
+        formlink += "<li class='atributo'>";
+        formlink += "<ul class='listalink'>";
+        formlink += "<li class='link'><a href='#'>xxx</a></li>";
+        formlink += "<li class='link'><a href='#'>xxx</a></li>";
+        formlink += "<li class='link'><a href='#'>xxx</a></li>";
+        formlink += "</ul>";
+        formlink += "</li>";
+        formlink += "</ul>";
+        formlink += "</section>";
+
+        var itemprojeto = document.getElementsByClassName("form -link");
+        itemprojeto = itemprojeto[itemprojeto.length - 1];
+        itemprojeto.innerHtml = formlink;
     }
 
-  
-   
+    var itemprojeto = document.getElementsByClassName("itemprojeto")
+    for (var i = 0; i < itemprojeto.length; i++) {
+        itemprojeto[i].onclick = function() { exibirUrl(this); }
+    }
+
+    var btnmais = document.getElementsByClassName("mais");
+    console.log(btnmais);
+    btnmais.onclick = function() { adicionaProjeto(); }
