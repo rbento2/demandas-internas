833a4c4ddbb2c60995fe3a440a2b5f1757ceb013
Renato Bento <renatombento@hotmail.com>
2017-05-15 15:12:26 -0300

    acertos de merge

--- src/operadoras/js/Claro.js
+++ src/operadoras/js/Claro.js
@@ -12,40 +12,6 @@
             itemprojeto[i].onclick = function() { exibirUrl(this); }
         }
     })();
-    
-    function adicionaProjeto() {
-       
-        var formlink = "<li class='itemprojeto -claro'>teste </li>";
-        formlink += "<section class='form -link'>";
-        formlink += "<ul class='listaatribo'>";
-        formlink += "<li class='atributo'>";
-        formlink += "<ul class='listalink'>";
-        formlink += "<li class='link'>Link de Dev </li>";
-        formlink += "<li class='link'>Link de Hmg </li>";
-        formlink += "<li class='link'>Link de Produção </li>"
-        formlink += "</ul>";
-        formlink += "</li>";
-        formlink += "<li class='atributo'>";
-        formlink += "<ul class='listalink'>";
-        formlink += "<li class='link'><a href='#'>xxx</a></li>";
-        formlink += "<li class='link'><a href='#'>xxx</a></li>";
-        formlink += "<li class='link'><a href='#'>xxx</a></li>";
-        formlink += "</ul>";
-        formlink += "</li>";
-        formlink += "</ul>";
-        formlink += "</section>";
-
-        console.log('------------------------------------');
-        console.log(formlink);
-        console.log('------------------------------------');
-        var itemprojeto = document.getElementsByClassName("itemprojeto -claro");
-        var i = itemprojeto.length - 1;
-        
-        itemprojeto[i].innerHtml = "<p>teste renato</p>";
-    }
-
-    
-
     var btnmais = document.getElementsByClassName("mais");
     console.log(btnmais[0]);
     btnmais[0].onclick = function() { adicionaProjeto(); }
